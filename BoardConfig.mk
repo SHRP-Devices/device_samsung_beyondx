ac3d4d481b70407a0d21b4cdd8a2bb5e5fdd8d12
corsicanu <me@corsicanu.ro>
2022-01-09 14:16:58 +0200

    recovery: introduce nuker, a valid replacement for multidisabler
    Signed-off-by: corsicanu <me@corsicanu.ro>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,19 +113,12 @@ SHRP_EXTERNAL_ADDON_2_BTN_TEXT := "Patch now"
 SHRP_EXTERNAL_ADDON_2_SUCCESSFUL_TEXT := "Successfuly patched"
 SHRP_INC_IN_REC_EXTERNAL_ADDON_2 := true
 
-SHRP_EXTERNAL_ADDON_3_NAME := "Samsung Multidisabler - v3.1"
-SHRP_EXTERNAL_ADDON_3_INFO := "Simple Samsung security services disabler"
-SHRP_EXTERNAL_ADDON_3_FILENAME := "multidisabler-samsung-3.1.zip"
-SHRP_EXTERNAL_ADDON_3_BTN_TEXT := "Patch now"
-SHRP_EXTERNAL_ADDON_3_SUCCESSFUL_TEXT := "Successfuly patched"
+SHRP_EXTERNAL_ADDON_3_NAME := "F2FS enabled fstab - only for Android 11 and 12"
+SHRP_EXTERNAL_ADDON_3_INFO := "Patched fstab to allow booting with f2fs formatted partitions"
+SHRP_EXTERNAL_ADDON_3_FILENAME := "f2fs_fstab_flashable.zip"
+SHRP_EXTERNAL_ADDON_3_BTN_TEXT := "Flash now"
+SHRP_EXTERNAL_ADDON_3_SUCCESSFUL_TEXT := "Successfuly flashed"
 SHRP_INC_IN_REC_EXTERNAL_ADDON_3 := true
-
-SHRP_EXTERNAL_ADDON_4_NAME := "F2FS enabled fstab - only for Android 11 and 12"
-SHRP_EXTERNAL_ADDON_4_INFO := "Patched fstab to allow booting with f2fs formatted partitions"
-SHRP_EXTERNAL_ADDON_4_FILENAME := "f2fs_fstab_flashable.zip"
-SHRP_EXTERNAL_ADDON_4_BTN_TEXT := "Flash now"
-SHRP_EXTERNAL_ADDON_4_SUCCESSFUL_TEXT := "Successfuly flashed"
-SHRP_INC_IN_REC_EXTERNAL_ADDON_4 := true
 
 # Include
 TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
